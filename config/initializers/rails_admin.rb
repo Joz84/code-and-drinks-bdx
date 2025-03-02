ee9e412f872a6ed08db6b7c7a63595a472d32bd7
Christopher Tavares <christavares33@gmail.com>
2025-03-02 03:07:12 +0100

    (feat): Remove action commented in initializer

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -50,33 +50,5 @@ RailsAdmin.config do |config|
     delete
     show_in_app
     update_round_status
-
-    # member :update_round_status do
-    #   only ["Round"]
-    #   link_icon "fas fa-wrench"
-
-    #   controller do
-    #     Proc.new do
-    #       @round = Round.find(params[:id])
-
-    #       if @round.active?
-    #         @round.create_last_games(finish: true)
-    #         @round.pasted!
-    #       elsif @round.pending?
-    #         @round.active!
-    #       end
-
-    #       redirect_to rails_admin.edit_path(model_name: "Round", id: @round.id)
-    #     end
-    #   end
-
-    #   visible do
-    #     bindings[:object].can_be_revealed? || bindings[:object].active?
-    #   end
-
-    #   register_instance_option :show_in_navigation do
-    #       false
-    #     end
-    # end
   end
 end
